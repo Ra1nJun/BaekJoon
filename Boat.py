66ff74bdab8c0f4fa8d8d719ec532cf1e76a19b5
Ra1nJun <Over4ction@gmail.com>
2025-12-15 16:32:16 +0900

    2025-12-15

--- Boat.py
+++ Boat.py
@@ -1,6 +1,5 @@
 # 1092 - 배 - gold 5
-import math
-
+# AI 도움 받음 : 각 크레인의 포인터를 따로 만들어서 들 수 없는 화물은 넘기고 찾기 시작함
 N = int(input())
 N_arr = list(map(int, input().split()))
 M = int(input())
@@ -11,16 +10,24 @@ M_arr.sort(reverse=True)
 
 if M_arr[0] > N_arr[0]:
     print(-1)
+else:
+    moved = [False] * M
+    cnt = M
+    time = 0
+    p = [0] * N
+    
+    while cnt > 0:
+        time += 1
+        for i in range(N):
+            j = p[i] 
+            while j < M:
+                if not moved[j] and N_arr[i] >= M_arr[j]:
+                    moved[j] = True
+                    cnt -= 1
+                    p[i] = j + 1
+                    break
+                j += 1
+            if j == M:
+                p[i] = j
 
-i, j, cnt = 0, 0, 0
-while i < N:
-    if j >= len(M_arr):
-        break
-    if N_arr[i] < M_arr[j]:
-        j += 1
-        continue
-    i += 1
-    j += 1
-    cnt += 1
-
-print(math.ceil(M/cnt))
+    print(time)
